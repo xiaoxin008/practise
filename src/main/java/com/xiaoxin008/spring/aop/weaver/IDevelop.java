128aa3a50f67b20a922c74f3bfbf888b2455900e
xiaoxin008 <313595055@qq.com>
2020-01-21 17:45:44 +0800

    feat(proxy):
    1.ProxyFactory原理

--- src/main/java/com/xiaoxin008/spring/aop/weaver/IDevelop.java
+++
@@ -1,3 +0,0 @@
-package com.xiaoxin008.spring.aop.weaver;
-
-
--- src/main/java/com/xiaoxin008/spring/aop/weaver/ITest.java
+++
@@ -1,7 +0,0 @@
-package com.xiaoxin008.spring.aop.weaver;
-
-public interface ITest {
-    default void test(){
-        System.out.println("测试....");
-    }
-}
